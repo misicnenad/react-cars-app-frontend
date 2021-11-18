8da662ce5fb8422786e373feda0ed352c29c9d54
shonmisic <n.misic@outlook.com>
2021-11-18 16:32:34 +0100

    Add default cars when backend not working

--- src/app/services/carService/index.ts
+++ src/app/services/carService/index.ts
@@ -2,13 +2,43 @@ import { apolloClient } from "../../graphql";
 import { GET_ALL_CARS } from "./queries";
 import { GetCars, GetCars_cars } from "./__generated__/GetCars";
 
+const testCar: GetCars_cars = {
+  __typename: "Car",
+  id: "ef3c1860-5ce6-47af-a13d-1ed72f65b642",
+  name: "Audi S3 Car",
+  mileage: "10k",
+  thumbnailUrl:
+    "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
+  dailyPrice: 70,
+  monthlyPrice: 1600,
+  gearType: "Auto",
+  gas: "Petrol",
+};
+const testCar2: GetCars_cars = {
+  __typename: "Car",
+  id: "ef3c1860-5ce6-47af-a13d-1ed72f65b641",
+  name: "HONDA CITY 5 Seater Car",
+  mileage: "20k",
+  thumbnailUrl:
+    "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
+  dailyPrice: 50,
+  monthlyPrice: 1500,
+  gearType: "Auto",
+  gas: "Petrol",
+};
+
 class CarService {
   public async getCars(): Promise<GetCars_cars[]> {
     const response = await apolloClient
       .query<GetCars>({ query: GET_ALL_CARS })
       .catch((err) => {
-        throw err;
-      });
+        // throw err;
+      })
+      .then(() => ({
+        data: {
+          cars: [testCar, testCar2, testCar, testCar2, testCar],
+        },
+      }));
 
     if (response && response.data && response.data.cars) {
       return response.data.cars;
